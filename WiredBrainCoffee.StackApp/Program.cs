87ef0ea95aecfffc99a2b84f92a502fa4e82ab76
Marian Marian <fenrir55@gmail.com>
2021-07-01 20:11:17 +0200

    Add project files.

---
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WiredBrainCoffee.StackApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stack = new SimpleStack();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            var sum = 0.0d;
+
+            while (stack.count > 0)
+            {
+                var item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Console.WriteLine($"Sum {sum}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WiredBrainCoffee.StackApp
+{
+    public class SimpleStack
+    {
+        private readonly double[] _items;
+        private int _currentIndex = -1;
+
+        public SimpleStack() => _items = new double[10];
+
+        public int count => _currentIndex + 1;
+
+        public double Pop() => _items[_currentIndex--];
+
+        public void Push(double item)
+        {
+            _currentIndex++;
+            _items[_currentIndex] = item;
+        }
+    }
+}
